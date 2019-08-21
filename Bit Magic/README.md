4d233008b00a073724d845ba679ebb6766b73543
anmistry <mistry_ankita@ymail.com>
2019-08-21 09:45:33 -0700

    This program performs bitshifting for entered integer and convert shifted integer value to binary format.

---
+++ Bit Magic/README.md
@@ -0,0 +1,15 @@
+Bit Magic Program: This program performs bitshifting for entered integer and convert shifted integer value to binary format.
+
+Requirements:
+1. User should provide input for an integer.
+2. User should provide position for bitshifting (either left or right).
+3. The final result for bitshifting than should convert it to binary format.
+
+How to build:
+1. git clone repository.
+2. Open Linux system.
+3. Change your directory where Bit Magic program is located.
+4.To complie the program, run command gcc bitMagic.c
+./a.out file should be created in the Projects folder.
+5. run ./a.out on command prompt.
+6. Program will execute. Follow user instructions in the program to get results.
---
+++ Bit Magic/bitMagic.c
@@ -0,0 +1,57 @@
+/*
+Author: Ankita Mistry
+Program: This program performs bitshifting for entered integer and convert shifted integer value to binary format.
+*/
+
+#include <stdio.h>
+
+int main(){
+	// Declare variables.
+	char pos;    // Store char value for bit shifing position.
+	int a;       // Stores the value of a for bitshifting operation.
+	int b;       // Stores the b value for bit shifing position.
+	int c[32];   // Stores the value for array for 32-bit binary number.
+	int i;       // Stores value loop counter.
+	int j;       // Stores value loop counter.
+	int result;  // Stores final result.
+
+	printf("Enter \n L for shifting Left \n R for shifting Right: ");
+	scanf("%c", &pos);
+	printf("Enter the value of an interger for bitshifting operation: ");
+	scanf("%d", &a);
+	printf("Enter the value by how many places you want to shift the integer: ");
+	scanf("%d", &b);
+
+	// Loop to perfrom task.
+	if(pos == 'L'){
+	//If selected position is left, than shift bits to left.
+		result = a << b;
+	}else if(pos == 'R'){
+	//If selected position is right, than shift bits to right.	
+		result = a >> b;
+	}else{
+	//For invalid input, print following message.
+		printf("Invalid input. Please enter L or R.\n");
+	}
+
+	printf("The shfited value is: %d\n", result);
+
+	// Convert decimal result value to binary number.
+	i = 0;
+	while (result > 0){
+		// Store modulus value of result in c[i] array (Store reaminder value.)
+		c[i] = (result % 2);
+		// Divide result in half, so modulus is 0 here.
+		result  = (result / 2);
+		i++;
+	}
+
+	printf("The decimal to binary conversation for shifted value is: ");
+	for(j = i - 1; j >= 0; j--){
+		printf("%d", c[j]);
+	}
+	
+	printf("\n");
+	
+	return 0;
+}
